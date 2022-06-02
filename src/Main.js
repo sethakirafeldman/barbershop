caa2f17f910b0f79a76f444c6d0f52ecf4b690bd
Seth Feldman <namdlefhtes@gmail.com>
2022-06-02 08:58:18 -0400

    start of layout and background

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import background from './assets/chair.jpg';
+
+export default function Main() {
+
+    return(
+        <main>
+            <section className='content-container'>
+            <p>Welcome to Sandro's</p>
+
+            </section>
+        </main>
+    )
+
+}
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 export default function Navbar () {
     return (
-        <nav>This is the Nav</nav>
+        <nav id='navigation-bar'>
+            <ul>
+              <li>Home</li>  
+              <li>About</li>
+              <li>Services</li>
+              <li>Booking</li>
+            </ul>
+        </nav>
     )
 
 }
