b427680ae539574345e8857484e8d055fe3299f0
namdlefhtes <namdlefhtes@gmail.com>
2022-06-02 22:05:41 -0400

    some changes

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 // import background from './assets/chair.jpg';
 
-export default function Main() {
+export default function Main(props) {
+
+    const [currentPage, setCurrentPage] = React.useState("home"); 
 
     return(
         <main>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 export default function Navbar () {
+
+    
+
+    const handleClick = () => {
+        console.log(event.target.value);
+    }
+
     return (
         <nav id='navigation-bar'>
-            <ul>
-              <li>Home</li>  
-              <li>About</li>
-              <li>Services</li>
-              <li>Booking</li>
-            </ul>
+            <div>
+              <button onClick = {handleClick} value="home">Home</button>  
+              <button onClick = {handleClick} value="about">About</button>
+              <button onClick = {handleClick} value="services">Services</button>
+              <button onClick = {handleClick} value="booking">Booking</button>
+            </div>
         </nav>
     )
-
 }
