53574d5427eb558e29ef2c5e254cd7ccc4a21a74
Seth Feldman <namdlefhtes@gmail.com>
2022-06-04 09:57:17 -0400

    added JSON for data

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import "./style.css"
 import Navbar from "./Navbar";
 import Main from './Main';
+const data = require('./data.json');
 
 export default function App(props) {
-
     const [currentPage, setCurrentPage] = React.useState("home");
-
+    
     const handleClick = () => {
         console.log(event.target.value);
         setCurrentPage(event.target.value);
     }
-
+    console.log(data[currentPage][0])
 
     return(
     <>
@@ -19,7 +19,10 @@ export default function App(props) {
             handleClick= {handleClick}
             currentPage = {currentPage}
         />
-        <Main current = {currentPage} />
+        <Main 
+            pageName = {data[currentPage][0].name}
+            text = {data[currentPage][0].text}
+        />
     </>
     )
 };
