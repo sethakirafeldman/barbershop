dec0b155eb9ee83ced0e41afe15743ecabcc0bdc
Seth Feldman <namdlefhtes@gmail.com>
2022-06-05 10:53:22 -0400

    dotenv-webpack config

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react';
+const data = require("./data.json");
 
 export default function Navbar (props) {
+    const genBtns = () => {
+        Object.values(data).map((item, index)=>{
+
+            console.log(item[0]);
+            return <button onClick = {props.handleClick} value={item[0].name}>{item[0].name}</button>
+        })
+    }
 
     return (
         <nav id='navigation-bar'>
-            <div>
+            <div id='nav-buttons'>
+            {/* {genBtns()} */}
               <button onClick = {props.handleClick} value="home">Home</button>  
               <button onClick = {props.handleClick} value="about">About</button>
               <button onClick = {props.handleClick} value="services">Services</button>
               <button onClick = {props.handleClick} value="booking">Booking</button>
+              <button onClick = {props.handleClick} value="location">Location</button>
             </div>
         </nav>
     )
