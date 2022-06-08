9875751f9c0a7f00148434743c4c91fcf856d087
Seth Feldman <namdlefhtes@gmail.com>
2022-06-08 10:04:25 -0400

    carousel button

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,17 +4,27 @@ import fade from "./assets/fade.jpg";
 import chevronR from "./assets/chevron-right.png";
 import chevronL from "./assets/chevron-left.png";
 
-export default function Carousel(){
-  
+export default function Carousel() {
+    const [index, setIndex] = React.useState(0);
+
+    const chevBtn = () => {
+        let parentEl = event.target.parentElement;
+        let activeImg = parentEl.children[1].children[0];
+        console.log(activeImg);
+        setIndex(activeImg.key);
+        console.log(index);
+        // change image as index increments.
+    };
+
     return (
         
         <div className='outer-carousel'>
-        <img src={chevronL} id="chevronL" /> 
-        <div className='carousel-container'>
-            <img src={beard} className="carousel-image active"/>
-            <img src={fade} className="carousel-image inactive"/>
-        </div>
-        <img src={chevronR} id="chevronR" />
+            <img src={chevronL} className="chevron" onClick={chevBtn}  /> 
+                <div className='carousel-container'>
+                    <img src={beard} className="carousel-image active" key={0}/>
+                    <img src={fade} className="carousel-image inactive" key={1}/>
+                </div>
+            <img src={chevronR} className="chevron" onClick={chevBtn} />
         </div>
     )
 }
