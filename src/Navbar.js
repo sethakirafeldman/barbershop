ec9acb22277c360d2eadf2c70f9dd0ac5beb1474
Seth Feldman <namdlefhtes@gmail.com>
2022-06-11 10:24:36 -0400

    menu items generate programmatically

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,22 +2,22 @@ import React from 'react';
 const data = require("./data.json");
 
 export default function Navbar (props) {
-    // const genBtns = () => {
-    //     Object.values(data).map((item, index) => {
-    //         console.log(item[0]);
-    //         return <button onClick = {props.handleClick} value={item[0].name}>{item[0].name}</button>
-    //     })
-    // }
+    const dataArr = Object.values(data);
+    let genBtns = dataArr.map((item, index) => {
+            // console.log(item[0]);
+            return (
+            <button 
+                key = {item[0].id} 
+                onClick = {props.handleClick} 
+                value={item[0].name.toLowerCase()}>{item[0].name}
+            </button>)
+        })
+  
 
     return (
         <nav id='navigation-bar'>
             <div id='nav-buttons'>
-            {/* {genBtns()} */}
-              <button onClick = {props.handleClick} value="home">Home</button>  
-              <button onClick = {props.handleClick} value="about">About</button>
-              <button onClick = {props.handleClick} value="services">Services</button>
-              <button onClick = {props.handleClick} value="booking">Booking</button>
-              <button onClick = {props.handleClick} value="location">Location</button>
+                {genBtns}
             </div>
         </nav>
     )
