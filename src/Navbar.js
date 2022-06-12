52e75f0effe4ee786c3f3eff01016de3fa2c5cb6
Seth Feldman <namdlefhtes@gmail.com>
2022-06-12 09:20:23 -0400

    working menu show

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import menuIcon from "../public/assets/menu_icon.png"
 const data = require("./data.json");
 
 export default function Navbar (props) {
     const dataArr = Object.values(data);
-    let genBtns = dataArr.map((item, index) => {
+    
+    const [showMenu, setShowMenu] = React.useState(false);
+
+    const genBtns = dataArr.map((item, index) => {
             // console.log(item[0]);
             return (
             <button 
@@ -12,12 +16,34 @@ export default function Navbar (props) {
                 value={item[0].name.toLowerCase()}>{item[0].name}
             </button>)
         })
-  
+    const toggleMenu = () => {
+        !showMenu ? setShowMenu(true) : setShowMenu(false);
+    }
 
     return (
         <nav id='navigation-bar'>
             <div id='nav-buttons'>
                 {genBtns}
+            </div>
+
+            <div id='hamburger-menu'>
+                <div id ="hamburger-items">
+                    <img 
+                        src={menuIcon} 
+                        className="hamburger-icon"
+                        onClick={toggleMenu}
+                    />
+                    <ul 
+                        className={showMenu ? "hamburger-list" : "hamburger-list hidden"}>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Booking</li>
+                        <li>Location</li>
+                    </ul>
+                    
+                   
+                </div>
             </div>
         </nav>
     )
